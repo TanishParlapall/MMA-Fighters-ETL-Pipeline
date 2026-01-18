6faa1a4b66d9f334721ba23b360b54c823670c04
Tanish Parlapall <tanish.parlapall@gmail.com>
2026-01-18 10:14:13 -0500

    Create dag.py

---
+++ dag/dag.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.standard.operators.python import PythonOperator
+from scripts.extract_data import fetch_MMA_data
+from scripts.create_table import create_table
+from scripts.load_data import load_into_postgres
+
+# default DAG args
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2026, 1, 16),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# DAG definition
+dag = DAG(
+    'fetch_and_store_MMA_data',
+    default_args=default_args,
+    description='first DAG',
+    schedule='@weekly',
+)
+
+# tasks
+fetch_MMA_data_task = PythonOperator(
+    task_id='fetch_MMA_data',
+    python_callable=fetch_MMA_data,
+    dag=dag,
+)
+
+create_table_task = PythonOperator(
+    task_id="create_table_task",
+    python_callable=create_table,
+    dag=dag,
+)
+
+insert_fighter_data_task = PythonOperator(
+    task_id="insert_fighter_data",
+    python_callable=load_into_postgres,
+    dag=dag,
+)
+
+# dependencies
+fetch_MMA_data_task >> create_table_task >> insert_fighter_data_task
